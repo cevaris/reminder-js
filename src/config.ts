22919c13ab7564cbee78dafa2a8afbd614595d0f
Adam Cardenas <cevaris@gmail.com>
2020-09-17 00:24:02 +0000

    init

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+class AppConfig {
+    REMINDER_EMAIL: string
+    REMINDER_EMAIL_PASSWORD: string
+
+    constructor() {
+        this.REMINDER_EMAIL = this.required('REMINDER_EMAIL');
+        this.REMINDER_EMAIL_PASSWORD = this.required('REMINDER_EMAIL_PASSWORD');
+    }
+
+    private required(name: string): string {
+        const value = process.env[name];
+        if (value) {
+            return value;
+        } else {
+            throw Error(`Missing ENV variable ${name}`)
+        }
+    }
+}
+
+export const Config = new AppConfig();
